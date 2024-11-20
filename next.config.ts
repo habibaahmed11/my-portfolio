587c44b24c8441148fc102dad98160f93131b416
habibaahmed11 <habibaahmed3372@gmail.com>
2024-11-20 04:57:24 +0200

    try againnn

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true
-  }
+  reactStrictMode: false,
+
 };
 
 export default nextConfig;
